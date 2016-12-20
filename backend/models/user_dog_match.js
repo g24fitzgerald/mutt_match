87009dd9d71e111a61845572a41a217de26d77f7
Gina Fitzgerald <g24fitzgerald@gmail.com>
2016-12-20 11:21:29 -0600

    made match model

--- backend/models/user_dog_match.js
+++ backend/models/user_dog_match.js
@@ -1,9 +1,24 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema,
+ObjectId = Schema.ObjectId;
 
 var schema = new mongoose.Schema({
+  playlist: {
+    type: ObjectId,
+    required: true,
+    ref: 'Preference',
+    index: true
+  },
+  dog: {
+    type: ObjectId,
+    required: true,
+    ref: 'Dog',
+    index: true
+  },
+  created: Date
 });
 
-var model = mongoose.model('Playlist', schema);
+var model = mongoose.model('userMatch', schema);
 
 // Make this available to our other files
 module.exports = model;
