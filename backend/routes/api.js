2323b2bf140f7ceb44e0875418ae453e83e8fc92
Gina Fitzgerald <g24fitzgerald@gmail.com>
2016-12-20 11:34:28 -0600

    backend etc

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,18 +3,18 @@ var router = express.Router();
 
 var UserPreference = require('../models/user_preference');
 var Dog = require('../models/dog');
-// var UserDogMatch = require('../models/user_dog_match');
+var UserMatch = require('../models/user_dog_match');
 
 //get all playlists for a user
-router.get('/dogs', function(req, res, next) {
+router.get('/profile', function(req, res, next) {
   //auth0's way of handling user
   // var userId = req.user.aud;
   // Find ALL documents that match the criteria passed, only return the "name" and "favorite" fields
-  console.log(userId);
+  // console.log(userId);
   Preference.find({ userId: req.user.aud }, '', function(err, result) {
     if (err) console.log(err);
-    console.log(userId);
-    console.log(result);
+    // console.log(userId);
+    // console.log(result);
 
     res.json(result);
   });
