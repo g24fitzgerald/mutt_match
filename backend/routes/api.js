7511ebb6551f042fed99550b19267f5cd8664667
Gina Fitzgerald <g24fitzgerald@gmail.com>
2016-12-19 19:04:23 -0600

    added api rout

---
+++ backend/routes/api.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+var UserPreference = require('../models/user_preference');
+var Dog = require('../models/dog');
+var UserDogMatch = require('../models/user_dog_match');
+
+//get all playlists for a user
+router.get('/user_preference', function(req, res, next) {
+  //auth0's way of handling user
+  // var userId = req.user.aud;
+  // Find ALL documents that match the criteria passed, only return the "name" and "favorite" fields
+
+  Playlist.find({ userId: req.user.aud }, '', function(err, preference) {
+    if (err) console.log(err);
+    console.log(preference);
+
+    res.json(preference);
+  });
+});
